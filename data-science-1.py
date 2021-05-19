777867a17152389d1b7073f0c0f8a043864cbc41
YogPanjarale <yog.panjarale@gmail.com>
2021-05-19 21:04:03 +0530

    sorting data done

--- data-science-1.py
+++ data-science-1.py
@@ -1,4 +1,5 @@
 import csv
+
 with open("data/given.csv","r") as f:
     r = csv.reader(f)
     rows = []
@@ -8,28 +9,43 @@ with open("data/given.csv","r") as f:
     planet_data_rows = rows[1:]
     # print(headers,"\n=========\n",planet_data_rows[0])
     planets_count = {}
-    # planets_in_star ={}
     for p in planet_data_rows:
         if planets_count.get(p[11]):
-            # planets_in_star[p[11]].append(p[1])
-            planets_count[p[11]]+=1
-        else:
-            # planets[p[11]]={
-            #     'planets':[p[11]],
-            #     'count':1
-            # }
-            # planets_in_star[p[11]]=[p[1]]
-            planets_count[p[11]]=1
-    print(planets_count)
-    max_planet = max(planets_count,lambda x: planets_count[x])
-    print(max_planet)
+            planets_count[p[11]]['count']+=1
+            planets_count[p[11]]['planets'].append(p[1])
+
+
+        else :
+            planets_count[p[11]]={
+                'count':1,
+                'planets':[p[1]]
+            }
+    max_planet = max(planets_count,key=lambda x: planets_count[x]['count'])
     # print(max_planet)
-    # print(planets[max_planet])
-
-    # hd_10180 = planets_in_star['hd 10180'.upper()]
+    
+    # hd_10180 = planets_count['hd 10180'.upper()]
     # print(hd_10180)
-
-    l =[]
-    #getting from one planet data
-    for i in planet_data_rows:
-        pass
+    print(len(planet_data_rows))
+    temp_planet_data = list(planet_data_rows)
+    for i in temp_planet_data:
+        planet_mass:str = i[3]
+        if planet_mass.lower()=="unknown":
+            planet_data_rows.remove(i)
+            continue
+        else:
+            remain = planet_mass.split(' ')[0]
+            t2 = planet_mass.split(' ')[1]
+            if t2 =="Jupiters":
+                remain=float(remain)*317.8
+                i[3]=remain
+        planet_radius = i[7]
+        if planet_radius.lower()=="unknown":
+            planet_data_rows.remove(i)
+            continue
+        else:
+            remain = planet_radius.split(' ')[0]
+            t2 = planet_radius.split(' ')[2]
+            if t2 =="Jupiter":
+                remain=float(remain)*11.2
+                i[7]=remain
+    print(len(planet_data_rows))
